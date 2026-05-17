d235c59428dd3b9173b0dd4376f2d42bb5de6b2c
esald <esaldanaf@gmail.com>
2026-05-17 16:08:54 +0200

    add github actions workflow for automated etl

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import sys
 from dotenv import load_dotenv
 from src.extract import BiziExtractor
 from src.transform import BiziTransformer
@@ -8,12 +9,18 @@ from src.load import BiziLoader
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+
 def main():
     load_dotenv()
 
     API_URL = "https://www.zaragoza.es/sede/servicio/urbanismo-infraestructuras/estacion-bicicleta.json"
     DB_URI = os.getenv("DATABASE_URL")
     TABLE_NAME = "bizi_stations"
+
+    from_date = sys.argv[1] if len(sys.argv) > 1 else None
+    to_date = sys.argv[2] if len(sys.argv) > 2 else None
+
+    logging.info("Starting ETL Process for Bizi Stations Data from_date: %s, to_date: %s", from_date, to_date)
 
     try:
         # E - Extract
